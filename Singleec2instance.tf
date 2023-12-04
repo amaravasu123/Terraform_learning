bb6046da90a454509ca7e509555f0f44438cca31
amaravasu <amaravasu123@gmail.com>
2023-12-04 19:58:28 +0530

    terraform first code

--- Singleec2instance.tf
+++ Singleec2instance.tf
@@ -1,1 +1,8 @@
-hi
+provider "aws" {
+    region = "ap-south-1"
+  
+}
+resource "aws_instance" "this" {
+  ami                     = "ami-0dcc1e21636832c5d"
+  instance_type           = "m5.large"
+}
